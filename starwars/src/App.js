7bf97f63217dcd958dc9b4f85ae8b185c3784fb6
AllHailMegatron <ahmadrthomp@gmail.com>
2019-06-24 21:06:48 -0400

    Completed CSS styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars LineUp!</h1>
+        <h1 className="Header">React Wars</h1>
         <CharacterList value={this.state.starwarsChars} />
       </div>
     );
--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -6,13 +6,13 @@ const Character = (props) => {
         <div className= "card-container">
             <div className="attr">
                 <div className= "char-name attr">{props.obj.name}</div> 
-                <div className= "char-height attr">{props.obj.height}</div>
-                <div className= "char-mass attr">{props.obj.mass}</div> 
-                <div className= "char-hair-color attr">{props.obj.hair_color}</div>
-                <div className= "char-skin-color attr">{props.obj.skin_color}</div>
-                <div className= "char-eye-color attr">{props.obj.eye_color}</div>
-                <div className= "char-birth-year attr">{props.obj.birth_year}</div>
-                <div className= "char-gender attr">{props.obj.gender}</div> 
+                <div><h2>Height: </h2> <span className= "char-height attr">{props.obj.height}</span></div>
+                <div><h2>Mass: </h2> <span className= "char-mass attr">{props.obj.mass}</span></div> 
+                <div><h2>Hair Color: </h2> <span className= "char-hair-color attr">{props.obj.hair_color}</span></div>
+                <div><h2>Skin Color: </h2> <span className= "char-skin-color attr">{props.obj.skin_color}</span></div>
+                <div><h2>Eye Color: </h2> <span className= "char-eye-color attr">{props.obj.eye_color}</span></div>
+                <div><h2>DOB</h2> <span className= "char-birth-year attr">{props.obj.birth_year}</span></div>
+                <div><h2>Gender</h2> <span className= "char-gender attr">{props.obj.gender}</span></div> 
             </div>
         </div>
     )
